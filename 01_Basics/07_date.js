0caae48c64e26f3235b9af466974a01afe95acc4
Amartya Kumar <107503167+amaartya2002@users.noreply.github.com>
2024-10-20 14:00:12 +0000

    date object

---
+++ 01_Basics/07_date.js
@@ -0,0 +1,12 @@
+
+const myDate = new Date()
+
+console.log(myDate); // 2024-10-20T13:50:32.850Z
+
+console.log(myDate.getDate());  // 20
+console.log(myDate.getDay()); // 0
+console.log(myDate.toDateString());  // Sun Oct 20 2024
+console.log(myDate.toString());   // Sun Oct 20 2024 13:52:39 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toLocaleString());  //  10/20/2024, 1:52:39 PM
+
+
